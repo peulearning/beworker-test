5f1c41093e7d1c30a610173ea892323ce88c7e71
Pedro <peuhenry2018@gmail.com>
2026-05-06 14:59:58 -0300

    Adicionando Rotas Para Links com Middleware 🚀

---
+++ src/modules/links/routes/link.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { LinkController } from '../controllers/link.controller';
+import { authMiddleware } from '../../../shared/middlewares/auth.middlewares';
+
+const router = Router();
+const controller = new LinkController();
+
+router.post('/', authMiddleware, controller.create);
+router.get('/', authMiddleware, controller.list);
+
+export default router;
